24e476c7a4b26b8d82c57ab529f141bf1702ad50
nashmaniac <nishantve1@gmail.com>
2015-07-28 19:38:22 +0530

    Testing new data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,6 @@
 (function() {
 	'use strict';
 
-	// let us see if we have the data here 
-	var MAX_DATA_LENGTH = 10000;
-
-	var truncatedData = sample_data.slice(0, MAX_DATA_LENGTH);
-
 	// The width and height properties of the map
 	var width = window.innerWidth,
 		height = window.innerHeight;
@@ -19,19 +14,13 @@
 		.attr('height', height);
 
 	var projection = d3.geo.equirectangular()
-		.scale(width/5.7)
-		.translate([width / 2, height / 2]);
-		// .precision(.1);
+		.scale(width / 5.7)
+		.translate([width / 2, height / 2])
+		.precision(.1);
 
 	var path = d3.geo.path()
 		.projection(projection);
 
-	var graticule = d3.geo.graticule();
-
-	svg.append("path")
-		.datum(graticule)
-		.attr("class", "graticule")
-		.attr("d", path);
 
 	d3.json("../scripts/world.json", function(error, world) {
 		if (error) throw error;
@@ -39,13 +28,6 @@
 		svg.insert("path", ".graticule")
 			.datum(topojson.feature(world, world.objects.land))
 			.attr("class", "land")
-			.attr("d", path);
-
-		svg.insert("path", ".graticule")
-			.datum(topojson.mesh(world, world.objects.countries, function(a, b) {
-				return a !== b;
-			}))
-			.attr("class", "boundary")
 			.attr("d", path);
 	});
 
@@ -55,23 +37,73 @@
 		var degPos = [];
 		degPos.push(longitude);
 		degPos.push(lattitude);
-		console.log(degPos);
 		return projection(degPos);
 	}
 
+	function randomRotateDeg(bottom, top) {
+		return Math.floor(Math.random() * (1 + top - bottom)) + bottom;
+	}
 	// Add the data points on the map
-	labels.selectAll('circle')
-		.data(truncatedData)
-		.enter()
-		.append('circle')
-		.classed('location', true)
-		.attr('r', '1px')
-		.each(function(d) {
-			var cartPos = getCartFromDeg(d.long, d.lat); // Cartesian position
-			d3.select(this).attr('cx', cartPos[0]);
-			d3.select(this).attr('cy', cartPos[1]);
+
+	// Data points rendered using svg images 
+
+	function addDataPointsTails(data) {
+		labels.selectAll('image')
+			.data(data)
+			.enter()
+			.append('image')
+			.attr('xlink:href', '../images/dot.svg')
+			.attr('width', '5px')
+			.attr('height', '5px')
+			.attr('opacity', 0)
+			.transition()
+			.duration(300).delay(100)
+			.attr('opacity', 1)
+			.each(function(d) {
+				var cartPos = getCartFromDeg(d[5], d[6]); // Cartesian position
+				d3.select(this)
+					.attr('x', cartPos[0] - 2.5)
+					.attr('y', cartPos[1] - 2.5)
+					.attr('transform', function(d) {
+						return 'rotate(' + randomRotateDeg(-180, 180) + ' ' + (cartPos[0] + 2.5) + ' ' + (cartPos[1] + 2.5) + ')';
+					});
+			});
+	}
+
+
+	function addDataPointsCircles(data) {
+		// Data points rendered using svg circles
+		labels.selectAll('circle')
+			.data(data)
+			.enter()
+			.append('circle')
+			.classed('location', true)
+			.attr('r', '5px')
+			.each(function(d) {
+				var cartPos = getCartFromDeg(d[5], d[6]); // Cartesian position
+				d3.select(this).attr('cx', cartPos[0]);
+				d3.select(this).attr('cy', cartPos[1]);
+			});
+	}
+
+
+	var dataUrl = "http://ec2-54-147-191-254.compute-1.amazonaws.com/view_relayer_dummy/get_views?callback=?";
+
+	function getJSON(url) {
+		return $.getJSON(url);
+	}
+
+	function getDatPointPromise(dataPointsUrl) {
+		console.log("Loading new data");
+		getJSON(dataPointsUrl).then(function(response) {
+			addDataPointsCircles(response);
+			console.log("loaded new data");
 		});
+	}
 
-
+	// set some amount of data or whatever right over here
+	setInterval(function() {
+		getDatPointPromise(dataUrl);
+	}, 2000);
 
 })();
