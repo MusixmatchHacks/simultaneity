87422aab18a10ee82f07f8df1df4a02a1bc860f7
nashmaniac <nishantve1@gmail.com>
2015-08-03 19:07:14 +0530

    Added font imports

--- bundle.js
+++ bundle.js
@@ -81,9 +81,9 @@
 	// jQuery Objects
 	var $topSongContainer = $('.top_song_container');
 
-	var osColorManager = __webpack_require__(3);
+	var osColorManager = __webpack_require__(2);
 
-	var WorldMap = __webpack_require__(2);
+	var WorldMap = __webpack_require__(4);
 	WorldMap.init();
 	var projection = WorldMap.getProjection();
 
@@ -245,69 +245,11 @@
 
 /***/ },
 /* 2 */
-/***/ function(module, exports) {
-
-	/**
-	 * Module that handles, rendering, positioning and everything on the world map. 
-	 * @namespace WorldMap
-	 */
-	'use strict';
-
-	var WorldMap = {
-		/**
-	  * Initializes and places the world map in the viewport
-	  * @memberof WorldMap
-	  * @method
-	  */
-		init: function init() {
-			this.width = window.innerWidth;
-			this.height = window.innerHeight;
-
-			this.labels = d3.select('#labels').attr('width', this.width).attr('height', this.height);
-
-			var svg = d3.select('#world').attr('width', this.width).attr('height', this.height);
-
-			this.projection = d3.geo.equirectangular().scale(this.width / 5.7).translate([this.width / 2, this.height / 2]).precision(.1);
-
-			var path = d3.geo.path().projection(this.projection);
-
-			d3.json("./vendors/world.json", function (error, world) {
-				if (error) throw error;
-
-				svg.insert("path", ".graticule").datum(topojson.feature(world, world.objects.land)).attr("class", "land").attr("d", path);
-			});
-
-			d3.select(self.frameElement).style("height", this.height + "px");
-		},
-
-		/**
-	  * Returns d3 projection, that can be used in other modules for caluclation related to positioning.
-	  * @memberof WorldMap
-	  * @method  getProjection
-	  */
-		getProjection: function getProjection() {
-			return this.projection;
-		},
-
-		/**
-	  * Returns an array holding width and height of the WorldMap
-	  * @memberof WorldMap
-	  * @method  getDimensions
-	  */
-		getDimensions: function getDimensions() {
-			return [this.width, this.height];
-		}
-	};
-
-	module.exports = WorldMap;
-
-/***/ },
-/* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
 
-	var sampleData = __webpack_require__(4);
+	var sampleData = __webpack_require__(3);
 
 	/**
 	 * A module to export functionality of retreiving various informations from a given requestId
@@ -418,7 +360,7 @@
 	module.exports = colorManager;
 
 /***/ },
-/* 4 */
+/* 3 */
 /***/ function(module, exports) {
 
 	module.exports = {
@@ -42561,6 +42503,64 @@
 	        "description": "Not categorized"
 	    }
 	}
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	/**
+	 * Module that handles, rendering, positioning and everything on the world map. 
+	 * @namespace WorldMap
+	 */
+	'use strict';
+
+	var WorldMap = {
+		/**
+	  * Initializes and places the world map in the viewport
+	  * @memberof WorldMap
+	  * @method
+	  */
+		init: function init() {
+			this.width = window.innerWidth;
+			this.height = window.innerHeight;
+
+			this.labels = d3.select('#labels').attr('width', this.width).attr('height', this.height);
+
+			var svg = d3.select('#world').attr('width', this.width).attr('height', this.height);
+
+			this.projection = d3.geo.equirectangular().scale(this.width / 5.7).translate([this.width / 2, this.height / 2]).precision(.1);
+
+			var path = d3.geo.path().projection(this.projection);
+
+			d3.json("./vendors/world.json", function (error, world) {
+				if (error) throw error;
+
+				svg.insert("path", ".graticule").datum(topojson.feature(world, world.objects.land)).attr("class", "land").attr("d", path);
+			});
+
+			d3.select(self.frameElement).style("height", this.height + "px");
+		},
+
+		/**
+	  * Returns d3 projection, that can be used in other modules for caluclation related to positioning.
+	  * @memberof WorldMap
+	  * @method  getProjection
+	  */
+		getProjection: function getProjection() {
+			return this.projection;
+		},
+
+		/**
+	  * Returns an array holding width and height of the WorldMap
+	  * @memberof WorldMap
+	  * @method  getDimensions
+	  */
+		getDimensions: function getDimensions() {
+			return [this.width, this.height];
+		}
+	};
+
+	module.exports = WorldMap;
 
 /***/ }
 /******/ ]);
